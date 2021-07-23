5b15f63a34791d473bb98433a575ca2a36938ed1
xil-max <925350125@qq.com>
2021-07-23 23:13:04 +0800

    2.2 exercise

---
+++ C++ Primer Plus/Chapter 2/2_2ex.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int distance;
+    cout << "please enter a distance number with long: ";
+    cin >> distance;
+    int res;
+    res = distance * 220;
+
+    cout << "tranformed with ma = " << res;
+    cin.get();
+    return 0;
+}
